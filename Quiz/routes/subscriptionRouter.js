4d27917089f8b59f4553cb7539e5494f453c358b
Risima Chabalala <186197922+Mshengu-R@users.noreply.github.com>
2026-02-18 11:05:42 +0200

    Refactor subscriptionRouter.js for clarity and structure

--- Quiz/routes/subscriptionRouter.js
+++ Quiz/routes/subscriptionRouter.js
@@ -1,3 +1,4 @@
+// routes with /:id requires the user to be having a certain id
 const express = require('express');
 const subRouter = express.Router();
 
@@ -22,17 +23,22 @@ subRouter.delete('/:id', (req,res)=>{
 })
 
 // the subscription for the specific user
+// requires and id for the user
 subRouter.get('/user/:id', (req,res)=>{
     res.json({message: "Get all user Subscriptions"})
 })
 
+// routes for canceling the subscription
+// requires and id for the user
 subRouter.put('/:id/cancel', (req,res)=>{
     res.json({message: "Cancel Subscription"})
 })
 
+// route for upcomming renewals
 subRouter.put('/upcomming-renewals', (req,res)=>{
     res.json({message: "Get upcomming renewals"})
 })
+
 
 
 module.exports = subRouter;
